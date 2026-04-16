cfe9a87cafa3a286d047108013e6196227bac366
LaraSantana27 <llarasantana27@gmail.com>
2026-04-16 15:50:35 -0300

    teste funções

---
+++ src/AtividadesType/Condicional/Questão1.ts
@@ -0,0 +1,6 @@
+// 1. Crie um programa que leia um número e informe se ele é:
+//  Par ou Ímpar
+//  Positivo ou Negativo
+
+let num = prompt("Informe um valor: ")
+let digito:number=Number(num)
---
+++ src/Aulas/funçãoVetores.ts
@@ -0,0 +1,21 @@
+let vetor:(string|number)[] = ["Lara","José"]
+
+console.log(vetor)
+
+//vetor.push(30)            //adicionar o elemento no final do vetor
+
+//vetor.pop()               //remove o elemento no final do vetor
+
+//vetor.unshift("Rosa")     //adicionar o elemento no inicio do vetor
+
+//vetor.shift()             //Remove o primeiro elemento do array
+
+//vetor.reverse()           //Inverte o vetor     
+
+//vetor.sort()              //Ordena em ordem alfabetica e crescente
+
+let paleta = vetor.join("-")
+
+
+//console.log(vetor)
+console.log(paleta)
